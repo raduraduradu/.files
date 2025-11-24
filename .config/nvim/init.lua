a416194b01f9ed889c8efbf972c4b448658cf1de
raduraduradu <radu.stanciu2016@gmail.com>
2025-11-24 20:00:44 +0200

    added nvim config file

---
+++ .config/nvim/init.lua
@@ -0,0 +1,14 @@
+-- theme and transparency
+vim.cmd('colorscheme zaibatsu')
+--vim.api.nvim_set_hl(0, 'Normal', { bg = 'none'})
+--vim.api.nvim_set_hl(0, 'NormalNC', { bg = 'none'})
+--vim.api.nvim_set_hl(0, 'EndOfBuffer', { bg = 'none' })
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.shiftwidth = 4
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.path:append("**")
+
+--binds
+
